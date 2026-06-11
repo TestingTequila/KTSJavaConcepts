5e8dc6a00606873306c99d73063e708cdcf007e6
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-06-11 06:57:18 +0530

    CallByValueVsCallByRefrence

---
+++ src/day19/CBV.java
@@ -0,0 +1,10 @@
+package day19;
+
+public class CBV
+{
+    public int addition(int a, int b)
+    {
+        int sum = a+b;
+        return sum;
+    }
+}
---
+++ src/day19/TestCBV.java
@@ -0,0 +1,10 @@
+package day19;
+
+public class TestCBV
+{
+    public static void main(String[] args) {
+       CBV cbv = new CBV();
+       int sum =cbv.addition(12,14);
+        System.out.println(sum);
+    }
+}
